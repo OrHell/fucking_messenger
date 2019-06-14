bf5dc2d2e2c915f2a75e0e9462934b766e593776
OrHell <43348516+OrHell@users.noreply.github.com>
2019-06-14 09:09:29 +0300

    Second Stage

--- get_pixel.py
+++ get_pixel.py
@@ -1,9 +1,25 @@
 from PIL import Image
-im = Image.open('1.png')
+im = Image.open('test.png')
 
-im.putpixel((9,9),(0,0,0,0))
+fuck_dict = {(0,0,0):'п',(255,0,0):'р',(0,16,255):'и',(50,205,50):'в',(97,5,15):'е',(52,58,5):'т'}
+k =0
+i = 0
+j = 0
+word_list = ''
+for i in range (0,20):
+	for j in range(0,20):
+		string_null = im.getpixel((i,j))
+		if string_null in fuck_dict:
+				word_list =word_list + str(fuck_dict[string_null] )
+print(word_list)
 
+
+
+
+#a= im.getpixel((1,1))
+#print(a)
 '''
-a= im.getpixel((0,0))
-print(a)
+string_null = str(string_null)
+		with open ('test.txt','a') as file:
+			file.write(string_null)
 '''
--- set_pixel.py
+++ set_pixel.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageDraw
 word = input(">")
 im = Image.open('1.png')
 
-first_dicts = {'п':(0,0,0,0),'р':(255,0,0),'и':(0,16,255),"в" : (50,205,50),'е':(97,5,15),'т':(52,58,5)}
+first_dicts = {'п':(0,0,0,245),'р':(255,0,0),'и':(0,16,255),"в" : (50,205,50),'е':(97,5,15),'т':(52,58,5)}
 
 word_list = list(word)
 print(first_dicts[word_list[0]])
@@ -11,7 +11,7 @@ count  = len(word_list)
 null_arg = 0
 
 for null_arg in range(0,count):
-	im.putpixel((1,null_arg),(first_dicts[word_list[null_arg]]))
+	im.putpixel((0,null_arg),(first_dicts[word_list[null_arg]]))
 	a= im.getpixel((1,1))
 print(a)
 
